a66da6892265cffcd30f8817dd48d7872adcdf78
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-03-02 10:37:33 +0100

    upload successful

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -5,56 +5,93 @@ import axios from 'axios'
 
 
     const Button = () => { 
-        const [ file, setFile ] = useState('')
+        const [ selectedFile, setselectedFile ] = useState('')
         const [ filename, setFilename ] = useState('Choose File')
 
         const chooseFile = (e) => {
-            setFile(e.target.files[0])
+            // console.log("object", e.target.files)
+            setselectedFile(e.target.files[0])
             setFilename(e.target.files[0].name)
         }
-
-        const handleUpload = async (e) => {
-            console.log("object from upload.................")
-            // const formData = new FormData();
-            // formData.append('file', file);
-
-            // console.log("file selected", file)
-
-            try {
-                const result = await axios({
-                    method: 'POST',
-                    url: `http://localhost:5000/upload`,
-                    // url: `https://www.googleapis.com/upload/drive/v3/files?uploadType=media`,
-                    // formData,
-                    // headers: {
-                //         // 'Authorization': `Bearer ${}`,
-                //         'Content-Type': 'image/jpeg',
-                //         'Content-Length':  500000
-                //     }
-                })
-                console.log("object33:::", result)
-            } catch (e) {
-                console.log("error::", e)
-            }            
+        
+        const onFormSubmit = (e) => {
+            e.preventDefault() // Stop form submit
+            handleUpload(selectedFile)
+                .then((response)=>{
+                    console.log(response, "my data");
+            })
         }
 
-        // const handleKey = (e) => {
-        //     console.log("object", e.target.name)
-        // }
+        const handleUpload = async (selectedFile) => {
+            const url = '/upload';
+            const formData = new FormData();
+            formData.append('file', selectedFile)
+            console.log("file selected", formData)
+            const config = {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            }
+            return  axios.post(url, formData,config)
+
+
+            // console.log("objecty", selectedFile)
+            // const formData = new FormData();
+            // formData.append('file', selectedFile);
+            // console.log("file selected", formData)
+
+            // try {
+                // const result = await axios({
+                //     method: 'POST',
+                //     url: `/upload`,
+                //     // url: `/upload?uploadType=media`,
+                //     // params: {
+                //     //     uploadType: media
+                //     // },
+                //     // data: selectedFile,
+                //      formData,
+                //     // headers: {
+                //     //     // 'Content-Type': 'application/json; charset=UTF-8'
+                //     //     'Content-Type': 'multipart/form-data'
+                //     //     // 'Content-Type': 'application/pdf',
+                //     //     // 'Content-Length': 50000
+                //     // }
+                //     headers: { 'content-type': 'multipart/form-data' },
+                //     // headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                    
+                    
+                    
+                //     // url: `https://www.googleapis.com/upload/drive/v3/files?uploadType=media`,
+                //     // formData,
+                //     // headers: {
+                // //         // 'Authorization': `Bearer ${}`,
+                // //         'Content-Type': 'image/jpeg',
+                // //         'Content-Length':  500000
+                // //     }
+                // })
+                // console.log("object33:::", result)
+            // } catch (e) {
+            //     console.log("error::", e)
+            // }            
+        }
     
         return ( 
             <div>
-                <div className="custom-file mb-3">
-                    <input type="file" className="custom-file-input" name="file" id="ctrl" onChange={chooseFile} />
-                    <label className="custom-file-label" htmlFor="customFile">{filename}</label>
-                </div>
-                <label for="telNo">Enter a telephone number (required): </label>
-                {/* <input id="telNo" name="telNo" type="tel" onKeyUp={handleKey} required/> */}
-                <span class="validity"></span>
+                <form onSubmit={onFormSubmit}>
+                    <div className="custom-file mb-3">
+                        <input type="file" className="custom-file-input" name="selectedFile" id="ctrl" onChange={chooseFile} />
+                        <label className="custom-file-label" htmlFor="customFile">{filename}</label>
+                    </div>
 
-                <div className="">
-                    <button type="submit" onClick={handleUpload}>Upload</button>
-                </div>
+                    {/* <div>
+                        <input type="file" />
+                    </div> */}
+
+                    <div className="">
+                        <button type="submit">Upload</button>
+                        {/* <button type="submit" onClick={handleUpload}>Upload</button> */}
+                    </div>
+                </form>
             </div>
          );
     }
